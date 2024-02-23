b7d3b87aecf31cbf5dc7216d0c76c9b8c74739e2
ChienHoang <hoangducchien97@gmail.com>
2024-02-23 17:25:03 +0700

    add layout

---
+++ src/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  Dispatch,
+  SetStateAction,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+type ThemeContextType = {
+  darkTheme: boolean;
+  setDarkTheme: Dispatch<SetStateAction<boolean>>;
+};
+
+const ThemeContext = createContext<ThemeContextType>({
+  darkTheme: true,
+  setDarkTheme: () => null,
+});
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const themeFromStorage: boolean =
+    typeof localStorage !== "undefined" && localStorage.getItem("hotel-theme")
+      ? JSON.parse(localStorage.getItem("hotel-theme")!)
+      : false;
+
+  const [darkTheme, setDarkTheme] = useState<boolean>(themeFromStorage);
+  const [renderComponent, setRenderComponent] = useState(false);
+
+  useEffect(() => {
+    setRenderComponent(true);
+  }, []);
+
+  if (!renderComponent) return <></>;
+
+  return (
+    <ThemeContext.Provider value={{ darkTheme, setDarkTheme }}>
+      <div className={`${darkTheme ? "dark" : ""} min-h-screen`}>
+        <div className="dark:text-white dark:bg-black text-[#1E1E1E]">
+          {children}
+        </div>
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext: () => ThemeContextType = () => {
+  const { darkTheme, setDarkTheme } = useContext(ThemeContext);
+  return {
+    darkTheme,
+    setDarkTheme,
+  };
+};
